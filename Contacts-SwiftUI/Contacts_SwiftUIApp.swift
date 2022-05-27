4b7a46dc62dcfd36f7c9db0590235e525eb8394d
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-05-27 17:47:05 +0700

    Initial Commit

---
+++ Contacts-SwiftUI/Contacts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_SwiftUIApp.swift
+//  Contacts-SwiftUI
+//
+//  Created by Алексей Синяговский on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
